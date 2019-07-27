72199fe70a7abf6c9e9f893eaa9e0979b93c015c
Paul Abernathy <paul.abernathy7@gmail.com>
2019-07-27 17:25:55 -0400

    both get requests work

--- index.js
+++ index.js
@@ -43,6 +43,25 @@ server.get("/api/users", (req, res) => {
     });
 });
 
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  Users.findById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved." });
+    });
+});
+
 server.listen(5000, () => {
   console.log("Server is listening on port 5000");
 });
