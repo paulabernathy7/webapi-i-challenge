acbd94e299bc8134ee2e41bb717516f268e5f9f0
Paul Abernathy <paul.abernathy7@gmail.com>
2019-07-28 12:04:57 -0400

    put works

--- index.js
+++ index.js
@@ -81,6 +81,35 @@ server.delete("/api/users/:id", (req, res) => {
     });
 });
 
+//update
+
+server.put("/api/users/:id", (req, res) => {
+  const { name, bio } = req.body;
+  const { id } = req.params;
+
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    Users.update(id, req.body)
+      .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({
+            message: "The user with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({
+          errorMessage: "The user information could not be modified."
+        });
+      });
+  }
+});
+
 server.listen(5000, () => {
   console.log("Server is listening on port 5000");
 });
